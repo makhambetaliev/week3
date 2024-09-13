f72f900e4fa1756cdfb0284c9ee7f410800374b8
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-09-13 12:34:19 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+//Problem 1
+//    int intNumber = 3;
+//    float floatNumber = 3.14;
+//    double doubleNumber = '3.1456586585';
+//    char charNumber = 'A';
+//    bool boolNumber = true;
+//
+//    cout << "Value of integer is: " << intNumber << " " << "size is: " << sizeof(intNumber) << endl;
+//    cout << "Value of float is: " << floatNumber << " " << "size is: " << sizeof(floatNumber)<< endl;
+//    cout << "Value of double is: " << doubleNumber << " " << "size is: " << sizeof(doubleNumber)<< endl;
+//    cout << "Value of char is: " << charNumber << " " << "size is: " << sizeof(charNumber)<< endl;
+//    cout << "Value of bool is: " << boolNumber << " " << "size is: " << sizeof(boolNumber)<< endl;
+//
+//    return  0;
+
+
+//Problem2
+//    std::cout << "Rasul Maxambetaliev\n" ;
+//    std::cout << "4005668\n";
+//    std::cout <<"Mirzo Ulugbek, mahat magandi 43\n";
+//    std::cout << "Anime cyberpunk\n";
+//    return  0;
+
+//Problem3
+//    cout << "\"I am standing\" on the edge of some crazy cliff.\n";
+//    cout << "What I have to do I have to catch everybody\\\n";
+//    cout << "if they start to go over the cliff--I mean\\\n";
+//    cout << "\\ if they are running and they dont ook\n";
+//    cout << "where they are going I have to cime out\n";
+//    cout << "from somewhere and \t catch them\n";
+
+//Problem4
+//    int a = 189;
+//    char b = 'B';
+//    cout<<a/4<<" ";
+//    cout<<int(b)<<" ";
+//    cout<<(a+int(b))/5<<" ";
+//    cout<<char((a/3-7)+int(b))<<" ";
+
+//Problem5
+//    cout<<"Sum= "<<12+38<<"\n";
+//    cout << "Product= "<<12*38<<"\n";
+
+//Problem6
+//    double a,b;
+//    cin>>a;
+//    cin>>b;
+//    cout<< "Sum: "<< a << '+'<<b<<"="<<a+b<<"\n";
+
+//Problem7
+//    double x,y;
+//    cin>>x;
+//    cin>>y;
+//    cout<< "t="<<((x*(x)+3)*3)/((y)/4+3)<<"\n";
+
+//Problem8
+//    double d;
+//    cin>>d;
+//    cout<<"Area: "<<3.14*d*d<<"\n";
+
+//Problem9
+//    double o,v,t;
+//    cin>>o;
+//    cin>>v;
+//    cin>>t;
+//    cout<<(v-o)/t<<"\n";
+
+//Problem10
+//    double a,b,c;
+//    cout<<"the distance to drive:\n";
+//    cin>>a;
+//    cout<<"the fuel efficiency of the car in miles per galon:\n";
+//    cin>>b;
+//    cout<<"the price per fallon: \n";
+//    cin>>c;
+//    cout<<"the cost for driving is $"<<a/b*c<<"\n";
+}
